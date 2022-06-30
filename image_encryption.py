b62468a49cb37dac4e25e9bd40a41afd4ed5dd06
Ilya Kotsar <57188064+ilyakotsar@users.noreply.github.com>
2022-06-30 10:38:08 +0700

    Update image_encryption.py

--- image_encryption.py
+++ image_encryption.py
@@ -1,90 +1,53 @@
 import numpy as np
+from itertools import cycle
 from PIL import Image
 
 
-def rail_fence_encrypt(text, key):
-    if 1 < key < len(text):
-        rail = [['\n' for i in range(len(text))] for j in range(key)]
-        dir_down = False
-        row, col = 0, 0
-        for i in range(len(text)):
-            if (row == 0) or (row == key - 1):
-                dir_down = not dir_down
-            rail[row][col] = text[i]
-            col += 1
-            if dir_down:
-                row += 1
-            else:
-                row -= 1
-        result = []
-        for i in range(key):
-            for j in range(len(text)):
-                if rail[i][j] != '\n':
-                    result.append(rail[i][j])
-        return result
-    else:
-        return text
+MAX_SIZE = 500
 
 
-def rail_fence_decrypt(cipher, key):
-    if 1 < key < len(cipher):
-        rail = [['\n' for i in range(len(cipher))] for j in range(key)]
-        dir_down = None
-        row, col = 0, 0
-        for i in range(len(cipher)):
-            if row == 0:
-                dir_down = True
-            if row == key - 1:
-                dir_down = False
-            rail[row][col] = '*'
-            col += 1
-            if dir_down:
-                row += 1
-            else:
-                row -= 1
-        index = 0
-        for i in range(key):
-            for j in range(len(cipher)):
-                if ((rail[i][j] == '*') and
-                   (index < len(cipher))):
-                    rail[i][j] = cipher[index]
-                    index += 1
-        result = []
-        row, col = 0, 0
-        for i in range(len(cipher)):
-            if row == 0:
-                dir_down = True
-            if row == key-1:
-                dir_down = False
-            if (rail[row][col] != '*'):
-                result.append(rail[row][col])
-                col += 1
-            if dir_down:
-                row += 1
-            else:
-                row -= 1
+def rail_pattern(n):
+    r = list(range(n))
+    return cycle(r + r[-2:0:-1])
+
+
+def rail_fence_encrypt(plaintext, rails):
+    if 1 < rails < len(plaintext):
+        p = rail_pattern(rails)
+        return sorted(plaintext, key=lambda i: next(p))
+    else:
+        return plaintext
+
+
+def rail_fence_decrypt(ciphertext, rails):
+    if 1 < rails < len(ciphertext):
+        p = rail_pattern(rails)
+        indexes = sorted(range(len(ciphertext)), key=lambda i: next(p))
+        result = [''] * len(ciphertext)
+        for i, c in zip(indexes, ciphertext):
+            result[i] = c
         return result
     else:
-        return cipher
+        return ciphertext
 
 
 def resize_image(im):
     x, y = im.size
     if x > y:
-        height = round(500 * y / x)
-        width = 500
+        height = round(MAX_SIZE * y / x)
+        width = MAX_SIZE
     else:
-        width = round(500 * x / y)
-        height = 500
+        width = round(MAX_SIZE * x / y)
+        height = MAX_SIZE
     im = im.resize((width, height), Image.ANTIALIAS)
     return im
 
 
 def get_pixels(im, width, height):
     colors = []
-    for r in range(0, width):
-        for c in range(0, height):
-            color = im.getpixel((r, c))
+    for x in range(width):
+        for y in range(height):
+            color = im.getpixel((x, y))
             colors.append(color)
     return colors
 
@@ -93,12 +56,12 @@ def generate_and_save_image(colors, width, height, filename):
     new_im = Image.new('RGB', (width, height))
     new_image = np.array(new_im)
     i = 0
-    for r in range(0, width):
-        for c in range(0, height):
-            re = colors[i][0]
-            gr = colors[i][1]
-            bl = colors[i][2]
-            new_image[c][r] = [re, gr, bl]
+    for x in range(width):
+        for y in range(height):
+            r = colors[i][0]
+            g = colors[i][1]
+            b = colors[i][2]
+            new_image[y][x] = (r, g, b)
             i += 1
     new_img = Image.fromarray(new_image, 'RGB')
     new_img.save(filename)
@@ -107,7 +70,7 @@ def generate_and_save_image(colors, width, height, filename):
 def encrypt_image(input_filename, output_filename, password):
     im = Image.open(input_filename)
     width, height = im.size
-    if width > 500 or height > 500:
+    if width > MAX_SIZE or height > MAX_SIZE:
         im = resize_image(im)
         width, height = im.size
     colors = get_pixels(im, width, height)
@@ -128,7 +91,7 @@ def decrypt_image(input_filename, output_filename, password):
     generate_and_save_image(decrypted, width, height, output_filename)
 
 
-PASSWORD = '2345678'
+PASSWORD = '432876523109687524169757'
 
 encrypt_image('filename.jpg', 'encrypted.png', PASSWORD)
 decrypt_image('encrypted.png', 'decrypted.png', PASSWORD)
